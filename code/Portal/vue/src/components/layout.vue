59eaa398db59f0882ed3d7bcc06661a0d69f6f66
Jared <jaredlam86@gmail.com>
2016-12-08 18:48:09 +0800

    added list page.

--- code/Portal/vue/src/components/layout.vue
+++ code/Portal/vue/src/components/layout.vue
@@ -1,20 +1,27 @@
 <template>
-    <div>
-        <el-row>
-            <el-col :span="24">
-                <el-menu theme="dark" default-active="1" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-                    <el-menu-item index="1">配置</el-menu-item>
-                    <el-menu-item index="2">历史消息查询</el-menu-item>
-                </el-menu>
-            </el-col>
-        </el-row>
-        <div class="grid-content">
-            <slot name="content">
-            </slot>
-        </div>
+  <div>
+    <el-row>
+      <el-col :span="24">
+        <el-menu theme="dark" default-active="1" class="el-menu-demo" mode="horizontal" @select="handleSelect" router="router">
+          <el-submenu index="1">
+            <template slot="title">Message</template>
+            <el-menu-item index="/">List</el-menu-item>
+            <el-menu-item index="/create">Create</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-col>
+    </el-row>
+    <div class="grid-content">
+      <slot name="content">
+      </slot>
     </div>
+  </div>
 </template>
 <style>
+    body{
+      margin:0px;
+    }
+
     .grid-content {
         padding:24px;
         height:auto;
@@ -22,13 +29,29 @@
         margin: 0 auto;
         position: relative;
     }
+
+    .el-menu-demo {
+        padding-left: 55px;
+    }
+
+
 </style>
 <script>
     export default {
+        data(){
+          return {
+            router:true
+          }
+        },
+
         methods: {
             handleSelect(key, keyPath) {
                 console.log(key, keyPath);
             }
         }
     }
+
+
+
+
 </script>

10e1f2f92930df5e77979c82b9824021a51548b1
js18 <jimmy.c.shi@yzw.cn>
2016-12-13 10:10:55 +0800

    增加WAR的打包方式

--- code/Portal/src/main/java/com/messagecenter/portal/Application.java
+++ code/Portal/src/main/java/com/messagecenter/portal/Application.java
@@ -2,10 +2,15 @@ package com.messagecenter.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
-
+public class Application extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
--- code/Readme.md
+++ code/Readme.md
@@ -5,12 +5,14 @@
 4.执行npm run dev, 然后访问http://localhost:8080
 
 配置JAVA开发环境
-1.设置环境变量MYSQL_HOST, RABBITMQ_HOST的DOCKER SERVER IP
+1.设置环境变量MYSQL_HOST, RABBITMQ_HOST到DOCKER SERVER IP
 2.Portal 项目端口 8081
 3.Sever 项目端口 8082
 
-发布说明
-Portal发布时，请在MAVEN中将Profiles选中PRD，这样会把vue生成的Js打包到JAR里面，然后直接访问http://localhost:8081，无需node环境。
+Portal发布说明
+1. 在MAVEN中将Profiles选中prd，这样会把vue生成的Js打包到JAR里面，然后直接访问http://localhost:8081，无需node环境。
+2. 在MAVEN中Profiles选中war，会将项目打包成war格式。
+
 
 使用Lombok来生成实体的getter/setter/toString/hashcode方法
 1.IntelliJ idea需要安装lombok插件(https://github.com/mplushnikov/lombok-intellij-plugin),请参考链接中的步骤进行配置

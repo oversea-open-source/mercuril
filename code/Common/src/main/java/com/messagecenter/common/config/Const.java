a10ae4156be5c732ba42f29c81ae79f933b4f8e8
Jared <jaredlam86@gmail.com>
2017-01-09 17:04:29 +0800

    fixed sql bug

--- code/Common/src/main/java/com/messagecenter/common/config/Const.java
+++ code/Common/src/main/java/com/messagecenter/common/config/Const.java
@@ -7,7 +7,7 @@ public class Const {
     public static final String EXCHANGE_NAME = "Mercuril-Exchange";
     public static final String IN_USER_NAME = "system";
 
-    public static final int MAX_RETRY_COUNT = 10;
+    public static final int MAX_RETRY_COUNT = 100;
 
     public static final String CLIENT_HOST = "http://localhost:8083";
     public static final String SERVER_HOST = "http://localhost:8082";
